d5e3b14a7a80ac1292e5b55cdf237d2ee7736b03
Brian Hall <bhall7@hawaii.edu>
2014-06-26 13:48:12 -1000

    added comments.py

---
+++ comments.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Welcome to comments.py. This program is full of comments. 
+
+# It hardly does anything at all.
+
+# In Python, comments start with the "#" character. When the computer is reading your program,
+# if it sees a "#" it ignores everything that comes after it -- until the next line.
+
+# I can put anything I want in comments, and it doesn't change the way the program runs.
+# Look -- I can even draw a cat picture:
+
+#      /\_/\
+#     ( o o )
+#     ==_Y_==
+#       `-'
+
+
+# Try running this program, then adding some comments of your own, then running it again.
+# Bonus: can you tell what the output of this program will be without running it?
+
+print("hello")
+#print("goodbye")
+
+
+
+# If you remove the "#" symbol at the beginning of the next five lines, the output of the 
+# program will change. Try it.
+
+#a = 5
+#b = 6
+#a = b + a
+#b = a
+#print(b)
